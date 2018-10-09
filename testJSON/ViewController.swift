27afe1aaa053b0d42fb8e0303363d3c8b7a3207b
Narongsak Launwijit <censpi@gmail.com>
2018-10-09 12:58:58 +0700

    mod Total Score

--- testJSON/ViewController.swift
+++ testJSON/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var totalTxt: UILabel!
+    
     var gold = [Gold]()
     
     var dataEntries: [ChartDataEntry] = []
@@ -25,6 +27,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var countArray:[String] = []
     var Date:[String] = []
     var countChart:[Double] = []
+    var sumCount:Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +35,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         tableView.delegate = self
         tableView.dataSource = self
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -59,17 +63,19 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
                     self.Date.append(i.date)
                 }
                 self.countChart = self.countArray.map { Double($0)!}
-                var sumCount:Double = 0
+                
                 for num in self.countChart {
-                    sumCount += num
+                    self.sumCount += num
                 }
                 
                 print(self.countArray)
                 print(self.Date)
                 print(self.countChart)
-                print(sumCount)
+                print(self.sumCount)
                 
-                self.setChart(values: self.countChart)
+                self.getTotal()
+//
+//                self.setChart(values: self.countChart)
                 self.tableView.reloadData()
                 
                 
@@ -92,66 +98,65 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return cell
     }
     
-//    func convertStringToDouble(value: [String]) -> [Double]{
-//        var a = Array<Double>!
-//        let a = value.flatMap(Double{$0})
-//        return a
+    func getTotal(){
+        totalTxt.text = String(sumCount)
+    }
+
+    //MARK: - Chart Setup View
+//    func setChart(values: [Double]){
+//        MChart.noDataText = "No data available!"
+//        for i in 0..<values.count {
+//            print("chart point : \(values[i])")
+//            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
+//            dataEntries.append(dataEntry)
+//        }
+//        let line1 = LineChartDataSet(values: dataEntries, label: "Units Consumed")
+//        line1.colors = [NSUIColor.blue]
+//        line1.mode = .cubicBezier
+//        line1.cubicIntensity = 0.2
+//
+//        let gredient = getGredientFilling()
+//        line1.fill = Fill.fillWithLinearGradient(gredient, angle: 90.0)
+//        line1.drawFilledEnabled = true
+//
+//        let data = LineChartData()
+//        data.addDataSet(line1)
+//        MChart.data = data
+//        MChart.setScaleEnabled(false)
+//        MChart.animate(xAxisDuration: 1.5)
+//        MChart.drawGridBackgroundEnabled = false
+//        MChart.xAxis.drawAxisLineEnabled = false
+//        MChart.xAxis.drawGridLinesEnabled = false
+//        MChart.leftAxis.drawAxisLineEnabled = false
+//        MChart.leftAxis.drawGridLinesEnabled = false
+//        MChart.rightAxis.drawAxisLineEnabled = false
+//        MChart.rightAxis.drawGridLinesEnabled = false
+//        MChart.legend.enabled = false
+//        MChart.xAxis.enabled = false
+//        MChart.leftAxis.enabled = false
+//        MChart.rightAxis.enabled = false
+//        MChart.xAxis.drawLabelsEnabled = false
+//
 //    }
-    
-    func setChart(values: [Double]){
-        MChart.noDataText = "No data available!"
-        for i in 0..<values.count {
-            print("chart point : \(values[i])")
-            let dataEntry = ChartDataEntry(x: Double(i), y: values[i])
-            dataEntries.append(dataEntry)
-        }
-        let line1 = LineChartDataSet(values: dataEntries, label: "Units Consumed")
-        line1.colors = [NSUIColor.blue]
-        line1.mode = .cubicBezier
-        line1.cubicIntensity = 0.2
-        
-        let gredient = getGredientFilling()
-        line1.fill = Fill.fillWithLinearGradient(gredient, angle: 90.0)
-        line1.drawFilledEnabled = true
-        
-        let data = LineChartData()
-        data.addDataSet(line1)
-        MChart.data = data
-        MChart.setScaleEnabled(false)
-        MChart.animate(xAxisDuration: 1.5)
-        MChart.drawGridBackgroundEnabled = false
-        MChart.xAxis.drawAxisLineEnabled = false
-        MChart.xAxis.drawGridLinesEnabled = false
-        MChart.leftAxis.drawAxisLineEnabled = false
-        MChart.leftAxis.drawGridLinesEnabled = false
-        MChart.rightAxis.drawAxisLineEnabled = false
-        MChart.rightAxis.drawGridLinesEnabled = false
-        MChart.legend.enabled = false
-        MChart.xAxis.enabled = false
-        MChart.leftAxis.enabled = false
-        MChart.rightAxis.enabled = false
-        MChart.xAxis.drawLabelsEnabled = false
-        
-    }
-    
-    private func getGredientFilling() -> CGGradient {
-        //TODO: - Setting fill gradien color
-        
-        let coloTop = UIColor(red: 141/255, green: 133/255, blue: 220/255, alpha: 1).cgColor
-        let coloBot = UIColor(red: 230/255, green: 155/255, blue: 210/255, alpha: 1).cgColor
-        
-        //TODO: - Color of gradient
-        
-        let gradientColors = [ coloTop,coloBot] as CFArray
-        
-        //TODO: - Positioning of the gradient
-        
-        let colorLocations: [CGFloat] = [ 0.7, 0.0 ]
-        
-        //TODO: - Gredient bject
-        
-        return CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations)!
-    }
+//
+//    private func getGredientFilling() -> CGGradient {
+//        //TODO: - Setting fill gradien color
+//
+//        let coloTop = UIColor(red: 141/255, green: 133/255, blue: 220/255, alpha: 1).cgColor
+//        let coloBot = UIColor(red: 230/255, green: 155/255, blue: 210/255, alpha: 1).cgColor
+//
+//        //TODO: - Color of gradient
+//
+//        let gradientColors = [ coloTop,coloBot] as CFArray
+//
+//        //TODO: - Positioning of the gradient
+//
+//        let colorLocations: [CGFloat] = [ 0.7, 0.0 ]
+//
+//        //TODO: - Gredient bject
+//
+//        return CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations)!
+//    }
 
 
 }
