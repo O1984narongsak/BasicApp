37814610e4b441bbec0756ccda07bab8c2333be4
Narongsak Launwijit <censpi@gmail.com>
2018-10-19 16:00:48 +0700

    bug Botton to OverView Chart

---
+++ testJSON/Controller/OverViewVC.swift
@@ -0,0 +1,149 @@
+//
+//  OverViewVC.swift
+//  testJSON
+//
+//  Created by Narongsak_O on 19/10/18.
+//  Copyright © 2018 nProject. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class OverViewVC: UIViewController {
+    
+//    weak var lineChartV: LineChartView!
+    @IBOutlet weak var lineChartV: LineChartView!
+    
+    var item = [String]()
+    var lineOne = [Double]()
+    var lineTwo = [Double]()
+    var lineX = [Double]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpMultiChart(name: item, vaule: lineOne, vauleT: lineTwo, vauleX: lineX)
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUpMultiChart(name:[String],vaule:[Double],vauleT:[Double],vauleX:[Double]){
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = Locale.current
+        let valuesNumberFormatter = ChartValueFormatter(numberFormatter: numberFormatter)
+        
+        lineChartV.dragEnabled = true
+        lineChartV.setScaleEnabled(true)
+        lineChartV.drawGridBackgroundEnabled = false
+        lineChartV.pinchZoomEnabled = true
+        lineChartV.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        lineChartV.borderColor = NSUIColor.white
+        lineChartV.borderLineWidth = 1.0
+        lineChartV.drawBordersEnabled = true
+        
+        //Mark: - xAxis
+        
+        let xAxis = lineChartV.xAxis
+        xAxis.labelFont = NSUIFont.systemFont(ofSize: CGFloat(12.0))
+        xAxis.labelTextColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        xAxis.drawGridLinesEnabled = true
+        xAxis.drawAxisLineEnabled = true
+        xAxis.labelPosition = .bottom
+        xAxis.labelRotationAngle = 0
+        xAxis.axisMinimum = 0
+        
+        lineChartV.chartDescription?.enabled = false
+        
+        var yVals1 = [ChartDataEntry]()
+        var yVals2 = [ChartDataEntry]()
+        var yVals3 = [ChartDataEntry]()
+        
+        for i in 0..<name.count {
+            let val = vaule[i]
+            yVals1.append(ChartDataEntry(x: Double(i), y: val))
+        }
+        
+        for i in 0..<name.count  {
+            let val = vauleT[i]
+            yVals2.append(ChartDataEntry(x: Double(i), y: val))
+        }
+        
+        for i in 0..<name.count  {
+            let val = vauleX[i]
+            yVals3.append(ChartDataEntry(x: Double(i), y: val))
+        }
+        
+        var set1 = LineChartDataSet()
+        var set2 = LineChartDataSet()
+        var set3 = LineChartDataSet()
+        
+        set1 = LineChartDataSet(values: yVals1, label: "Income")
+        set1.valueFormatter = valuesNumberFormatter
+        set1.valueFormatter = set2.valueFont.withSize(CGFloat(8.0)) as? IValueFormatter
+        set1.axisDependency = .left
+        set1.colors = [#colorLiteral(red: 0.215686274509804, green: 0.709803921568627, blue: 0.898039215686275, alpha: 1.0)]
+        set1.circleColors = [NSUIColor.purple]
+        set1.lineWidth = 2.0
+        set1.circleRadius = 3.0
+        set1.fillAlpha = 65 / 255.0
+        set1.fillColor = #colorLiteral(red: 0.215686274509804, green: 0.709803921568627, blue: 0.898039215686275, alpha: 1.0)
+        set1.highlightColor = NSUIColor.green
+        set1.highlightEnabled = true
+        set1.drawCircleHoleEnabled = true
+        
+        
+        set2 = LineChartDataSet(values: yVals2, label: "Payment")
+        set2.valueFormatter = valuesNumberFormatter
+        set2.valueFormatter = set2.valueFont.withSize(CGFloat(7.0)) as? IValueFormatter
+        set2.axisDependency = .right
+        set2.colors = [NSUIColor.red]
+        set2.circleColors = [NSUIColor.orange]
+        set2.lineWidth = 2.0
+        set2.circleRadius = 3.0
+        set2.fillAlpha = 65 / 255.0
+        set2.fillColor = NSUIColor.red
+        set2.highlightColor = NSUIColor.white
+        set2.highlightEnabled = true
+        set2.drawCircleHoleEnabled = true
+        
+        set3 = LineChartDataSet(values: yVals3, label: "gap")
+        set3.valueFormatter = valuesNumberFormatter
+        set3.valueFormatter = set2.valueFont.withSize(CGFloat(10.0)) as? IValueFormatter
+        set3.axisDependency = .right
+        set3.colors = [NSUIColor.green]
+        set3.circleColors = [NSUIColor.white]
+        set3.lineWidth = 2.0
+        set3.circleRadius = 3.0
+        set3.fillAlpha = 65 / 255.0
+        set3.fillColor = NSUIColor.green
+        set3.highlightColor = NSUIColor.yellow
+        set3.highlightEnabled = true
+        set3.drawCircleHoleEnabled = true
+        
+        //TODO: - append line
+        var dataSets = [LineChartDataSet]()
+        dataSets.append(set1)
+        dataSets.append(set2)
+        dataSets.append(set3)
+        
+        let data = LineChartData(dataSets: dataSets)
+        data.setValueTextColor(#colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1))
+        data.setValueFont(NSUIFont(name: "HelveticaNeue-Light", size: CGFloat(9.0)))
+        lineChartV.data = data
+        
+        
+        
+        //        lineChartV.data?.notifyDataChanged()
+        //        lineChartV.notifyDataSetChanged()
+        
+        //TODO: - animate Chart
+        
+        lineChartV.animate(xAxisDuration: 2, easingOption: .easeInBounce)
+        lineChartV.animate(yAxisDuration: 2, easingOption: .easeInBounce)
+        
+    }
+    
+
+
+
+}
--- testJSON/Controller/StatVC.swift
+++ testJSON/Controller/StatVC.swift
@@ -12,22 +12,46 @@ class StatVC: UIViewController ,UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var overBtn: UIButton!
+    
     var name = ["mgd_month","mtk_month","beauty_month","jew_month"]
     var brand = ["MTK Racing","MTK TECH","MTK Beauty","MTK Jew"]
-//    var image = []
+
     var incomeS: Double = 0
     var payS:Double = 0
     var gapS:Double = 0
+//    var incomAS = [Double]()
+//    var payAS = [Double]()
     var incomArray = [Double]()
     var payArray = [Double]()
     
+    var incomATwo = [Double]()
+    var payATwo = [Double]()
+    
+    var incomAT = [Double]()
+    var payAT = [Double]()
+    
+    var incomAFo = [Double]()
+    var payAFo = [Double]()
+    
+    var plus = [Double]()
+    var fill = [Double]()
+    var gapFP = [Double]()
+    var clear = [Double]()
+    var nameC:[String] = []
+    
+    var fillS : String = ""
+    var plusS : String = ""
+    var gapFpS : String = ""
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         downloadJson()
+        overBtn.layer.cornerRadius = 10.0
 
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -57,14 +81,16 @@ class StatVC: UIViewController ,UITableViewDelegate, UITableViewDataSource {
         vc?.image = UIImage(named: name[indexPath.row])!
         
         self.navigationController?.pushViewController(vc!, animated: true)
+        
+        
     }
     
     func downloadJson(){
-//        let url = URL(string:urlP)
-//        print(urlP)
+
         for a in name {
             print(a)
             let urlB = "https://office.mtkserver.com/result_account/\(a)/08/2018"
+            print(urlB)
             let url = URL(string:urlB)
             guard let downloadURL = url else { return }
             URLSession.shared.dataTask(with: downloadURL) { (data, urlResponse, error) in
@@ -72,48 +98,107 @@ class StatVC: UIViewController ,UITableViewDelegate, UITableViewDataSource {
                     print("Something wrong")
                     return }
                 print("downloaded")
-                //            print(data)
+
                 do{
                     let decoder = JSONDecoder()
                     let actors = try decoder.decode([Stat].self, from: data)
+                    
                     for i in actors{
-                        self.incomeS += Double(i.m_in)!
-                        self.payS += Double(i.m_out)!
-                        if self.incomArray.count == 0 && self.payArray.count == 0 {
+//                        if self.incomArray.count == 0 && self.payArray.count == 0 {
                             self.incomArray.append(Double(i.m_in)!)
                             self.payArray.append(Double(i.m_out)!)
-                        } else {
-
-                            print(self.incomArray[0])
-
-                        }
-                        //                  MARK: - Keep Dynamic data in Array      
-//                        if self.incomArray == nil  && self.payArray == nil{
-//                            self.incomArray.append(Double(i.m_in)!)
-//                            self.payArray.append(Double(i.m_out)!)
-//                        }else {
-//                            self.incomArray += (Double(i.m_in)!)
-//                            self.payArray += (Double(i.m_out)!)
-//                        }
-
+                        
                     }
                     
-                    //TODO: - get Income and Payment
-                    print(self.incomeS)
-                    print(self.payS)
-                    print(self.incomeS - self.payS)
-//                    self.tableView.reloadData()
+                    print("pay \(self.payArray.count)")
+                    print("income \(self.incomArray.count)")
+
+                    if self.plus.count == 0 {
+                        self.plus = self.payArray
+                    } else if self.plus.count < self.payArray.count {
+                        let a = self.payArray.count - self.plus.count
+                        let b = Array(repeating: 0.0, count: a)
+                        self.plus += b
+                        for(index,_) in self.plus.enumerated(){
+                            self.plus[index] = self.plus[index] + self.payArray[index]
+                        }
+                    } else if self.plus.count > self.payArray.count {
+                        let a = self.plus.count - self.payArray.count
+                        let b = Array(repeating: 0.0, count: a)
+                        self.payArray += b
+                        for(index,_) in self.plus.enumerated(){
+                            self.plus[index] = self.plus[index] + self.payArray[index]
+                        }
+                        print("plus\(self.plus.count)")
+                    }
+
+                    if  self.fill.count == 0 {
+                        self.fill = self.incomArray
+                    }else if self.fill.count < self.incomArray.count {
+                        let c = self.incomArray.count - self.fill.count
+                        let d = Array(repeating: 0.0, count: c)
+                        self.fill += d
+                        for(index,_) in self.fill.enumerated(){
+                            self.fill[index] = self.fill[index] + self.incomArray[index]
+                        }
+                    } else if self.fill.count > self.incomArray.count {
+                        let c = self.fill.count - self.incomArray.count
+                        let d = Array(repeating: 0.0, count: c)
+                        self.incomArray += d
+                        for(index,_) in self.fill.enumerated(){
+                            self.fill[index] = self.fill[index] + self.incomArray[index]
+                        }
+                    }
+
+                    self.incomArray.removeAll()
+                    self.payArray.removeAll()
+
+                    let t = self.plus.reduce(0, { ($0 + $1)})
+                    let h = self.fill.reduce(0, { ($0 + $1)})
+                    self.nameC = Array(repeating: "1", count: self.plus.count)
+                    
+                    for gap in self.fill {
+                        for a in self.plus{
+                            self.gapFP.append(gap - a)
+                        }
+                    }
+                    
+                    print(self.nameC)
+                    print(self.plus.count)
+                    print(self.fill.count)
+                    print("plus\(t)")
+                    print("fill\(h)")
+                    
+                    self.fillS = String(t)
+                    self.plusS = String(h)
+                    self.gapFpS = String(h - t)
+                    
+                    self.tableView.reloadData()
                 } catch {
                     print("something wrong after downloaded")
                 }
                 }.resume()
-        }
-        
+        } //for loop
+    
     }
     
-
-
-
+    
+    @IBAction func toOverBtn(_ sender: UIButton) {
+        performSegue(withIdentifier: "toOverView", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toOverView" {
+            let secondVC = segue.destination as! OverViewVC
+            secondVC.item = nameC
+            secondVC.lineOne = fill
+            secondVC.lineTwo = plus
+            secondVC.lineX = gapFP
+            
+        }
+    }
+    
+    
 }
 
 
